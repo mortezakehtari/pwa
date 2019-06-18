3d94c1885c7467fadb3d458b6565819a5fef1597
mortezakehtari <morteza.kehtari@gmail.com>
2019-06-18 20:35:22 +0430

    added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
+const apikey = '2b0f2f81ce354dcfac0b216ed32b66b8';
+const main = document.querySelector('main');
+const defaultSource = "aftenposten";
+
 window.onload = () => {
   'use strict';
 
+  updateNews();
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
              .register('./sw.js');
   }
+
+}
+
+function updateNews(source = defaultSource){
+  const res = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apikey}`);
+  const json =  await res.json();
+
+  main.innerHTML = json.articles.map(createArticle).join("\n")
+  
+}
+
+
+function createArticle(article){
+  return `
+  <div class = "article">
+      <a href="${article.url}">
+          <h2>${article.title}</h2>
+          <img src="${article.urlToImage}" alt="${article.title}">
+          <p>${article.description}</p>
+      </a>
+  </div>
+  `;
 }
