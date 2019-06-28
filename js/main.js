ed501cba62186510972a79428fb8464c6a7844b7
mortezakehtari <morteza.kehtari@gmail.com>
2019-06-28 18:48:25 +0430

    update 2

--- js/main.js
+++ js/main.js
@@ -27,6 +27,7 @@ if('serviceWorker' in navigator){
   // .then(data => {
   //   main.innerHTML = data.articles.map(createArticle).join("\n")
   // });
+  
                 $.ajax({
                     type: "GET",
                     url: `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apikey}`,
