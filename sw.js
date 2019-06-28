6d282b75ba0e3bd0c7652ecbe0e72a858ba66502
mortezakehtari <morteza.kehtari@gmail.com>
2019-06-28 21:08:01 +0430

    added

--- sw.js
+++ sw.js
@@ -4,42 +4,45 @@ var filesToCache = [
   '/index.html',
   '/css/style.css',
   '/js/main.js',
+  '/js/jquery.js',
   '/manifest.json',
   './fallback.json',
+    './images/not-connected.jpg'
 ];
 
+
 self.addEventListener('install', async event => {
-  const cache = await caches.open('new-static');
-  cache.addAll(filesToCache);
-  });
-  
-  self.addEventListener('fetch', event => {
-     const req = event.request;
-     const url = new URL(req.url);
-  
-     if(url.origin == location.origin){
-         event.respondWith(cacheFirst(req));
-     }
-     else{
-         event.respondWith(networkFirst(req));
-     }
-  
-  });  
-  
-  async function cacheFirst(req){
-     const cacheResponse = await caches.match(req);
-     return cacheResponse || fetch(req);
-  }
-  
-  async function networkFirst(req){
-     const cache =await caches.open('new-dynamic')
-     try{
-         const res = await fetch(req);
-         cache.put(req,res.clone());
-         return res;
-     }
-     catch(error){
-         const cachedResponse =  await cache.match(req);
-         return cachedResponse || await caches.match('./fallback.json');
-     }
-  }
+    const cache = await caches.open('new-static');
+    cache.addAll(filesToCache);
+    });
+    
+    self.addEventListener('fetch', event => {
+       const req = event.request;
+       const url = new URL(req.url);
+    
+       if(url.origin == location.origin){
+           event.respondWith(cacheFirst(req));
+       }
+       else{
+           event.respondWith(networkFirst(req));
+       }
+    
+    });  
+    
+    async function cacheFirst(req){
+       const cacheResponse = await caches.match(req);
+       return cacheResponse || fetch(req);
+    }
+    
+    async function networkFirst(req){
+       const cache =await caches.open('new-dynamic')
+       try{
+           const res = await fetch(req);
+           cache.put(req,res.clone());
+           return res;
+       }
+       catch(error){
+           const cachedResponse =  await cache.match(req);
+           return cachedResponse || await caches.match('./fallback.json');
+       }
+    }
